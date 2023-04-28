0eddbc36efe7a7f2657d6545d07ac9fcbfa2c9dc
Dylan Lukes <lukes.dylan@gmail.com>
2023-04-28 13:03:23 -0700

    disable D3D shadergen on Linux, support older git path

--- cmake/bgfx.cmake
+++ cmake/bgfx.cmake
@@ -2,7 +2,7 @@ FetchContent_Declare (
         bgfx
         GIT_REPOSITORY "https://github.com/bkaradzic/bgfx.cmake.git"
         GIT_TAG "v1.118.8417-420"
-        PATCH_COMMAND git reset --hard && git apply --ignore-space-change --ignore-whitespace --allow-empty "${PROJECT_SOURCE_DIR}/cmake/patches/bgfx-cmake.patch"
+        PATCH_COMMAND git reset --hard && git apply --ignore-space-change --ignore-whitespace "${PROJECT_SOURCE_DIR}/cmake/patches/bgfx-cmake.patch"
         ${NEOPAD_COMMON_DEPS_OPTIONS}
 )
 set(BGFX_BUILD_TOOLS ON CACHE INTERNAL "")
